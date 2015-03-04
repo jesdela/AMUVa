b5f2f6f5ae57f0f819cdf17bed0b396be1f95aaa
Jesús Diego de la Fuente <jdiego122253@gmail.com>
2015-03-04 02:18:03 +0100

    Primera versión estable

--- app/src/main/java/com/jldes/amuva/TabClasificacion.java
+++ app/src/main/java/com/jldes/amuva/TabClasificacion.java
@@ -1,9 +1,25 @@
 package com.jldes.amuva;
 
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by JesúsDiego on 03/03/2015.
  */
-public class TabClasificacion extends Fragment{
+public class TabClasificacion extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_clasificaciones,container,false);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(),"Comfortaa-Regular.ttf");
+        TextView titulo = (TextView)v.findViewById(R.id.titulo_clasi);
+        titulo.setTypeface(typeface);
+
+        return v;
+    }
 }
